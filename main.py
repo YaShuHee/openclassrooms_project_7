2141b037763ccb87795b9a2a00024caaeb051874
Timothé Gélibert <tim.gelibert@protonmail.com>
2021-07-27 10:43:34 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+
+import argparse
+
+
+def extract_dataset():
+    pass
+
+
+def create_groups():
+    pass
+
+
+def calculate_group_profit():
+    pass
+
+
+def treat_dataset():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
