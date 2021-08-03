45aa58fb4f74a63df35a9165460e4413f761d280
Timothé Gélibert <tim.gelibert@protonmail.com>
2021-08-03 23:41:10 +0200

    Fixes #7, few code optimisation

--- utils.py
+++ utils.py
@@ -1,12 +1,23 @@
-from functools import cached_property
-import pandas
+from abc import ABC
 
+import pandas
 
 # Displays int XOR floats rounded to two decimal places.
 pandas.options.display.float_format = lambda n: str(int(n)) if int(n) == n else "{:.2f}".format(n)
 
 
-class Action:
+class Serializable(ABC):
+    """ An abstract class that can be serialized. """
+    @property
+    def serialized(self):
+        to_serialize = ["name", "price (€)", "profit (€)", "price_by_profit"]
+        return {
+            key.capitalize().replace("_", " "): object.__getattribute__(self, key.replace(" (€)", ""))
+            for key in to_serialize
+        }
+
+
+class Action(Serializable):
     """ An object to access to actions properties easily. """
     def __init__(self, name: str, price: str, profit: str):
         self.name = name
@@ -18,13 +29,8 @@ class Action:
     def __str__(self):
         return f"{self.name}({self.price}€) => +{self.profit}€"
 
-    @cached_property
-    def serialized(self):
-        to_serialize = ["name", "price (€)", "profit (€)", "price_by_profit"]
-        return {key.capitalize(): object.__getattribute__(self, key.replace(" (€)", "")) for key in to_serialize}
 
-
-class Group:
+class Group(Serializable):
     """ An object to access a resume of several actions, their total cost and total profit. """
     def __init__(self, actions: list[Action]):
         self.actions = actions
@@ -34,30 +40,25 @@ class Group:
 
     def __str__(self):
         data = [action.serialized for action in sorted(self.actions, key=lambda a: a.price_by_profit)]
+        self.name = "TOTAL"
         data.append(self.serialized)
         df = pandas.DataFrame(data)
         return df.to_string(index=False)
 
-    @cached_property
-    def serialized(self):
-        to_serialize = ["price (€)", "profit (€)", "price_by_profit"]
-        serialized = {key.capitalize(): object.__getattribute__(self, key.replace(" (€)", "")) for key in to_serialize}
-        serialized["Name"] = "TOTAL"
-        return serialized
+    def _sum_attribute(self, key):
+        """ Make the sum of an attribute from all actions. """
+        sum_ = 0
+        for action in self.actions:
+            sum_ += object.__getattribute__(action, key)
+        return sum_
 
     def calculate_price(self):
         """ Calculate an actions group total price. """
-        price = 0
-        for action in self.actions:
-            price += action.price
-        return price
+        return self._sum_attribute("price")
 
     def calculate_profit(self):
         """ Calculate an actions group total profit. """
-        profit = 0
-        for action in self.actions:
-            profit += action.profit
-        return profit
+        return self._sum_attribute("profit")
 
     def calculate_price_by_profit(self):
         """ Calculate an actions group price by profit. """
